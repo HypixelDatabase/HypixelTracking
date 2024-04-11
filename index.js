26778affde03f3b65200cbd20014b1df9e23ef82
builder_247 <14019974+builder-247@users.noreply.github.com>
2024-04-11 16:55:05 +0300

    blacklist `oldItem`

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const whiteListedArrays = [
   'tutorial', 'visited_zones', 'crafted_minions', 'achievement_spawned_island_types', 'unlocked_coll_tiers'
 ];
 
-const blacklistedValues = ['contract_choices', 'currently_upgrading', 'bats_spawned'];
+const blacklistedValues = ['contract_choices', 'currently_upgrading', 'bats_spawned', 'oldItem'];
 
 function uniqueArrayElements(target, source) {
   const destination = target.slice().concat(source);

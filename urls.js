53bc97de5be8104a830f5f89026459a42d19e289
Kaeso <24925519+ptlthg@users.noreply.github.com>
2024-06-27 12:53:32 -0500

    Add more top players to list (#23)
    * Add Chissl/Lime to profiles list (one of the top farmers)
    
    * (Optional) Add 2 more top players
    
    * (optional) Add Nemqnja

--- urls.js
+++ urls.js
@@ -29,6 +29,10 @@ module.exports = [
       '1277d71f338046e298d90c9fe4055f00', // 56ms/Strawberry
       '7c207917505f4b48bab6631f2953151f', // Dueces/Kiwi
       'a7da9276d9fa49a18e770a0509584780', // TheOriginalAce/Mango
+      'aea28adee98d489eb582638d20e80e23', // Chissl/Lime
+      'd5c8118f053e412680b5ed66bf71066e', // HuTayo/Mango
+      '5825e8f071d04806b92687d79b733f30', // 15h/Mango
+      'fad90e13ff44441c8c5e1dbe489fd44f', // Nemqnja/Tomato
     ],
     transform: (obj, { merge, uniqueArrayElements }) => {
       let uuid = {};
@@ -56,6 +60,10 @@ module.exports = [
       '1277d71f338046e298d90c9fe4055f00', // 56ms/Strawberry
       '7c207917505f4b48bab6631f2953151f', // Dueces/Kiwi
       'a7da9276d9fa49a18e770a0509584780', // TheOriginalAce/Mango
+      'aea28adee98d489eb582638d20e80e23', // Chissl/Lime
+      'd5c8118f053e412680b5ed66bf71066e', // HuTayo/Mango
+      '5825e8f071d04806b92687d79b733f30', // 15h/Mango
+      'fad90e13ff44441c8c5e1dbe489fd44f', // Nemqnja/Tomato
     ],
     transform: (obj, { merge, uniqueArrayElements }) => {
       let uuid = {};

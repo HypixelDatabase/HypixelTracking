20e36d5e531e15a7057d10eee1bbf0d7a6a44160
Jacob <kathund@proton.me>
2026-05-24 16:18:34 +0800

    1 files | player.json (#35)
    * split: clean object
    
    I got sick of doing this every time I wanted to manually update stuff so
    I'm just leaving it this time
    
    * 1 files | player.json

---
+++ cleanObject.js
@@ -0,0 +1,72 @@
+const whiteListedArrays = [
+  'packages',
+  'cakes_found_by_name',
+  'unlockedColors',
+  'unlockedParts',
+  'custom_titles',
+  'bridgeMapWins',
+  'tutorial',
+  'visited_zones',
+  'crafted_minions',
+  'achievement_spawned_island_types',
+  'unlocked_coll_tiers'
+];
+
+const blacklistedValues = ['contract_choices', 'currently_upgrading', 'bats_spawned', 'oldItem'];
+
+function containsBadKey(key) {
+  return (
+    blacklistedValues.includes(key) ||
+    key.startsWith('fetchur-') ||
+    key.startsWith('spooky_festival_') ||
+    key.startsWith('layout_items_') ||
+    key.startsWith('claimed_solo_bank_') ||
+    key.startsWith('claimed_coop_bank_') ||
+    key.startsWith('given_cookies_') ||
+    /\d+:\d+_\d+:/.test(key) ||
+    /^[0-9a-fA-F]{32}$/.test(key)
+  );
+}
+
+function normalizeObject(object) {
+  const o = { ...object };
+  const keys = Object.keys(o).filter((key) => {
+    if (containsBadKey(key)) {
+      delete o[key];
+      return false;
+    }
+    return true;
+  });
+  keys.forEach((key) => {
+    let entry = o[key];
+    switch (typeof entry) {
+      case 'number':
+        entry = 0;
+        break;
+      case 'string':
+        entry = '';
+        break;
+      case 'boolean':
+        entry = true;
+        break;
+      default:
+        if (Array.isArray(entry)) {
+          if (!whiteListedArrays.includes(key)) {
+            if (typeof entry[0] === 'object') {
+              entry = [{ ...normalizeObject(entry[0]) }];
+              break;
+            }
+            entry = [];
+          }
+        } else if (entry === null) {
+          entry = '';
+        } else {
+          entry = normalizeObject(entry);
+        }
+    }
+    o[key] = entry;
+  });
+  return o;
+}
+
+module.exports = { containsBadKey, normalizeObject };
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const merge = require('deepmerge');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const urls = require('./urls');
-const path = require("path");
+const path = require('path');
+const { normalizeObject } = require('./cleanObject.js');
 
 require('dotenv').config();
 const apiKey = process.env.HYPIXEL_API_KEY || '';
@@ -17,80 +18,21 @@ const logprefix = {
   info: '[ INFO\x1b[0m ]',
   ok: '[  \x1b[32mOK\x1b[0m  ]',
   warn: '[ \x1b[33mWARN\x1b[0m ]',
-  error: '[\x1b[31mFAILED\x1b[0m]',
+  error: '[\x1b[31mFAILED\x1b[0m]'
 };
 
 function log(msg = '', level = 'info', timer) {
   let timeStr = '';
   if (timer) {
     const stop = process.hrtime(timer);
-    timeStr = `\x1b[32m+${Math.round((stop[0] * 1e9 + stop[1])/1e6)}ms\x1b[0m`
+    timeStr = `\x1b[32m+${Math.round((stop[0] * 1e9 + stop[1]) / 1e6)}ms\x1b[0m`;
   }
   console.log(`${logprefix[level]} ${msg} ${timeStr}`);
 }
 
-const whiteListedArrays = [
-  'packages', 'cakes_found_by_name', 'unlockedColors', 'unlockedParts', 'custom_titles', 'bridgeMapWins',
-  'tutorial', 'visited_zones', 'crafted_minions', 'achievement_spawned_island_types', 'unlocked_coll_tiers'
-];
-
-const blacklistedValues = ['contract_choices', 'currently_upgrading', 'bats_spawned', 'oldItem'];
-
 function uniqueArrayElements(target, source) {
   const destination = target.slice().concat(source);
   return [...new Set(destination)];
-}
-
-function containsBadKey(key) {
-  return (blacklistedValues.includes(key))
-    || key.startsWith('fetchur-')
-    || key.startsWith('spooky_festival_')
-    || key.startsWith('layout_items_')
-    || key.startsWith('claimed_solo_bank_')
-    || key.startsWith('claimed_coop_bank_')
-    || /\d+:\d+_\d+:/.test(key)
-    || /^[0-9a-fA-F]{32}$/.test(key);
-}
-
-function normalizeObject(object) {
-  const o = { ...object  };
-  const keys = Object.keys(o).filter((key) => {
-    if (containsBadKey(key)) {
-      delete o[key];
-      return false;
-    }
-    return true;
-  })
-  keys.forEach(key => {
-    let entry = o[key];
-    switch (typeof entry) {
-      case 'number':
-        entry = 0;
-        break;
-      case 'string':
-        entry = '';
-        break;
-      case 'boolean':
-        entry = true;
-        break;
-      default:
-        if (Array.isArray(entry)) {
-          if (!whiteListedArrays.includes(key)) {
-            if (typeof entry[0] === 'object') {
-              entry = [{...normalizeObject(entry[0])}]
-              break;
-            }
-            entry = [];
-          }
-        } else if (entry === null) {
-          entry = '';
-        } else {
-          entry = normalizeObject(entry);
-        }
-    }
-    o[key] = entry;
-  })
-  return o;
 }
 
 async function get(url) {
@@ -100,7 +42,7 @@ async function get(url) {
     log(`Fetched ${url}`, 'ok');
     return response.data;
   } catch (error) {
-    log(`Fetching ${url}: ${error}`,'error');
+    log(`Fetching ${url}: ${error}`, 'error');
   }
 }
 
@@ -128,7 +70,7 @@ const tasks = [
 
         try {
           if (Object.hasOwn(entry, 'transform')) {
-            results = results.map((result) => entry.transform(result, { merge, uniqueArrayElements }))
+            results = results.map((result) => entry.transform(result, { merge, uniqueArrayElements }));
           }
 
           if (results.length > 1) {
@@ -146,17 +88,20 @@ const tasks = [
             const { lastUpdated } = require(path);
 
             if (lastUpdated > obj.lastUpdated) {
-              log(`Resource '${type}' returned data from ${lastUpdated}, which is older than ${obj.lastUpdated}! Discarding...`, 'warn');
+              log(
+                `Resource '${type}' returned data from ${lastUpdated}, which is older than ${obj.lastUpdated}! Discarding...`,
+                'warn'
+              );
               continue;
             }
           }
 
           await fs.promises.writeFile(path, JSON.stringify(obj, null, 2));
         } catch (error) {
-          log(`Failed resource ${type}: ${error}`, 'error')
+          log(`Failed resource ${type}: ${error}`, 'error');
         }
       }
-    },
+    }
   },
   {
     name: 'Discard trivial changes',
@@ -170,7 +115,8 @@ const tasks = [
       const data = await mc.ping({ host: 'mc.hypixel.net', version: '1.8.9' });
       data.players.online = 0;
       data.latency = 0;
-      data.description = data.description.replace(/\d DAYS? [\d]{2}:[\d]{2}:[\d]{2}/g, 'd DAYS HH:MM:SS')
+      data.description = data.description
+        .replace(/\d DAYS? [\d]{2}:[\d]{2}:[\d]{2}/g, 'd DAYS HH:MM:SS')
         .replace(/[\d]{2}:[\d]{2}:[\d]{2}/g, 'HH:MM:SS');
       return fs.writeFileSync('./ServerListPing/slp.json', JSON.stringify(data, null, 2));
     }
@@ -180,7 +126,7 @@ const tasks = [
     func: async () => {
       log('Starting Puppeteer...');
       const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
       const page = await browser.newPage();
       await page.goto('https://api.hypixel.net', { waitUntil: 'networkidle2' });
@@ -188,7 +134,7 @@ const tasks = [
       const client = await page.target().createCDPSession();
       await client.send('Page.setDownloadBehavior', {
         behavior: 'allow',
-        downloadPath: path.resolve('./API/'),
+        downloadPath: path.resolve('./API/')
       });
 
       const downloadSelector = 'a[download="swagger.json"]';
@@ -199,16 +145,16 @@ const tasks = [
       await wait(1000);
       await browser.close();
       try {
-        await fs.promises.rename('./API/swagger.json', './API/docs.json')
+        await fs.promises.rename('./API/swagger.json', './API/docs.json');
       } catch (e) {
         // idc
       }
       try {
-        await fs.promises.unlink('./API/swagger.json.crdownload')
+        await fs.promises.unlink('./API/swagger.json.crdownload');
       } catch (e) {
         //
       }
-    },
+    }
   }
 ];
 
@@ -216,19 +162,20 @@ async function doTask(task) {
   const { name } = task;
   log(`Running task ${name}`);
   const start = process.hrtime();
-  return task.func()
+  return task
+    .func()
     .then(() => log(`Finished task ${name}`, 'ok', start))
-    .catch((error) => log(`Failed task ${name}: ${error}`, 'error', start))
+    .catch((error) => log(`Failed task ${name}: ${error}`, 'error', start));
 }
 
 (async () => {
   const start = Date.now();
   log('Started HypixelTracking script');
   if (apiKey.length === 0) {
-    log('No API key set!', 'warn')
+    log('No API key set!', 'warn');
   }
   for (const task of tasks) {
     await doTask(task);
   }
-  log(`Finished all tasks in ${(Date.now() - start) / 1000}s`)
+  log(`Finished all tasks in ${(Date.now() - start) / 1000}s`);
 })();
